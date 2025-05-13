cb2c6b93f4e41f363b869deedf9d6005e58fdd22
Chiheb Hmida <chiheb.hmida@gmail.com>
2025-05-13 15:50:33 +0200

    build: add mongo db connection

--- apps/graphql/data/resolvers.js
+++ apps/graphql/data/resolvers.js
@@ -1,6 +1,7 @@
-
+import { Widgets } from './dbConnectors';
+const mongoose = require('mongoose');
 class Product {
-  constructor(id, { name, price, description, soldout,inventory, stores }) {
+  constructor(id, { name, price, description, soldout, inventory, stores }) {
     this.id = id;
     this.name = name;
     this.price = price;
@@ -13,14 +14,46 @@ class Product {
 
 const productDatabase = {};
 
-
 const resolvers = {
-    getProduct: ({id}) => {return new Product (id, productDatabase[id] ) },
-    createProduct: ({ input }) => {
-    let id = require("crypto").randomBytes(10).toString("hex");
-    productDatabase[id] = input;
-    return new Product(id, input);
-  },
-}
+  /*  getProduct: ({ id }) => {
+    return new Product(id, productDatabase[id]);
+  }, */
+  getProduct: async ({ id }) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new Error('Invalid product ID format');
+      }
 
-export default resolvers
+      const product = await Widgets.findById(id);
+      if (!product) {
+        throw new Error('Product not found');
+      }
+
+      return product;
+    } catch (error) {
+      throw new Error(`Failed to get product: ${error.message}`);
+    }
+  },
+  createProduct: async ({ input }) => {
+    /*     let id = require('crypto').randomBytes(12).toString('hex'); */
+    const id = new mongoose.Types.ObjectId();
+    const newProduct = new Widgets({
+      _id: id,
+      name: input.name,
+      price: input.price,
+      description: input.description,
+      soldout: input.soldout,
+      inventory: input.inventory,
+      stores: input.stores,
+    });
+
+    await newProduct.save();
+
+    return {
+      id: id.toHexString(),
+      ...input,
+    };
+  },
+};
+
+export default resolvers;
